93e3e22ee2c19a44d621885e6964f36188534a86
Misha Causur <mishacausur@mail.ru>
2025-01-14 21:26:52 +0500

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+package ru.netology
+
+import kotlin.math.max
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val amount = 1200
+
+    val feePercent = (amount * 0.75) / 100
+
+    val minFeeAmount = 35
+
+    val fee = max(
+        feePercent.toInt(),
+        minFeeAmount
+    )
+
+    println("Комиссия за перевод составит $fee руб.")
+}
