a4706a6e3489104472c7e89887dca7565340ae3c
Misha Causur <mishacausur@mail.ru>
2025-01-14 21:27:43 +0500

    task 1 has been extracted into single method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,10 @@ import kotlin.math.max
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    task1()
+}
+
+fun task1() {
     val amount = 1200
 
     val feePercent = (amount * 0.75) / 100
