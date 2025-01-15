460d720f9b59de5d9e392cd619fdef14911e80b8
Misha Causur <mishacausur@mail.ru>
2025-01-15 21:51:34 +0500

    task 3 done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,8 @@ package ru.netology
 
 import kotlin.math.max
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    task2()
+    task3()
 }
 
 fun task1() {
@@ -32,4 +30,32 @@ fun task2() {
     val result = if (likes.toString().last() == '1') single else plural
 
     println("Понравилось $likes $result")
+}
+
+fun task3() {
+    val isLoyalCustomer = true
+    val amount = 10004
+    var result = 0
+//    if (amount in 0..1_000) {
+//        result = amount
+//    } else if (amount in 1_001..10_000) {
+//        result = amount - 100
+//    } else if (amount > 10_000) {
+//        result = amount - (amount * 5 / 100)
+//    } else {
+//        result = 0
+//    }
+
+    when (amount) {
+        in 0..1_000 -> result = amount
+        in 1_001..10_000 -> result = amount - 100
+        in 10_001..Int.MAX_VALUE -> result = amount - (amount * 5 / 100)
+        else -> result = 0
+    }
+
+    if (isLoyalCustomer) {
+        result = result - (result * 1) / 100
+    }
+
+    println("Итоговая стоимость покупки составляет $result руб.")
 }
