6f6dce27c59f1a55f568613444854aa3111291b4
Misha Causur <mishacausur@mail.ru>
2025-01-30 20:16:59 +0500

    post model added

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,37 +1,18 @@
 package ru.netology
 
-import kotlin.math.abs
-import kotlin.math.max
+data class Post(
+    val id: UInt,
+    val ownerId: UInt,
+    val date: Int,
+    val text: String,
+    val views: UInt,
+    val markedAsAds: Boolean,
+    val likes: UInt,
+    val canLike: Boolean,
+    val userLiked: Boolean,
+    val canPublish: Boolean,
+)
 
 fun main() {
-    //task2()
-}
 
-enum class CardType {
-    MIR, VISA, MASTERCARD
-}
-
-fun calculateCommission(cardType: CardType = CardType.MIR, pastTransferAmount: Int = 0, amount: Int): Int {
-    when (cardType) {
-        CardType.MIR -> return 0
-        CardType.VISA -> return max(((amount * 0.75) / 100).toInt(), 35)
-        CardType.MASTERCARD -> return calculateMasterCardCommission(pastTransferAmount, amount)
-    }
-}
-
-fun calculateMasterCardCommission(pastTransferAmount: Int, amount: Int): Int {
-    val limit = 75_000
-    when {
-        pastTransferAmount > limit -> return getMCCommission(amount).toInt()
-        (pastTransferAmount + amount) > limit -> {
-            val residue = limit - pastTransferAmount
-            return getMCCommission(amount - residue).toInt()
-        }
-
-        else -> return 0
-    }
-}
-
-fun getMCCommission(amount: Int): Double {
-    return ((abs(amount) * 0.6) / 100) + 20
 }
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -5,48 +5,4 @@ import ru.netology.calculateCommission
 
 class MainKtTest {
 
-    // MASCTERCARD
-    @Test
-    fun calculateMasterCardOverLimit() {
-        val commission = 320 // 50_000 * 6% + 20 rub
-        val result = calculateCommission(CardType.MASTERCARD, amount = 125_000)
-        assertEquals(commission, result)
-    }
-
-    @Test
-    fun calculateMasterCardOverPassLimit() {
-        val commission = 290 // 45_000 * 6% + 20 rub
-        val result = calculateCommission(CardType.MASTERCARD, 125_000, 45_000)
-        assertEquals(commission, result)
-    }
-
-    @Test
-    fun calculateMasterCardLessLimit() {
-        val commission = 0 // 25_000 < 75_000 -> 0
-        val result = calculateCommission(CardType.MASTERCARD, amount = 25_000)
-        assertEquals(commission, result)
-    }
-
-    // VISA
-    @Test
-    fun calculateVisaOverConstant() {
-        val commission = 187 // 25_000 * 0.75% = 187
-        val result = calculateCommission(CardType.VISA, amount = 25_000)
-        assertEquals(commission, result)
-    }
-
-    @Test
-    fun calculateVisaLEssConstant() {
-        val commission = 35 // 1_500 * 0.75% < 35 -> 35
-        val result = calculateCommission(CardType.VISA, amount = 1_500)
-        assertEquals(commission, result)
-    }
-
-    // MIR
-    @Test
-    fun calculateMir() {
-        val commission = 0
-        val result = calculateCommission(CardType.MIR, amount = 10_500)
-        assertEquals(commission, result)
-    }
 }
