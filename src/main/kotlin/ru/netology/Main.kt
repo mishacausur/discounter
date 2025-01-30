9c39129c6a7b22e4fd10fe2ee4e7ed7ba43cbdf0
Misha Causur <mishacausur@mail.ru>
2025-01-30 20:31:39 +0500

    wall service added

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,5 +1,7 @@
 package ru.netology
 
+import java.util.function.BiPredicate
+
 data class Post(
     val id: UInt,
     val ownerId: UInt,
@@ -11,7 +13,37 @@ data class Post(
     val canLike: Boolean,
     val userLiked: Boolean,
     val canPublish: Boolean,
+    val comments: Comments,
 )
+
+data class Comments(
+    val count: UInt,
+    val canPost: Boolean,
+    val canClose: Boolean,
+    val canOpen: Boolean
+)
+
+object Wall {
+    var id: UInt = 0u
+    private var posts = emptyArray<Post>()
+
+    fun addPost(post: Post): Post {
+        id += 1u
+        val _post = post.copy(id = id)
+        posts += _post
+        return posts.last()
+    }
+
+    fun updatePost(post: Post): Boolean {
+        for ((index, _post) in posts.withIndex()) {
+            if (_post.id == post.id) {
+                posts[index] = post
+                return true
+            }
+        }
+        return false
+    }
+}
 
 fun main() {
 
