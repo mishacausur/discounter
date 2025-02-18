81a196c0e2d8b36c206836968035d192b0e1b42a
Misha Causur <mishacausur@mail.ru>
2025-02-18 21:14:53 +0500

    messages started

--- src/main/kotlin/ru/netology/Messages.kt
+++ src/main/kotlin/ru/netology/Messages.kt
@@ -1,7 +1,14 @@
 package ru.netology
 
-data class Message(val id: UInt) {}
+data class Message(
+    val id: UInt,
+    var text: String
+    ) {}
 
+data class Group(
+    val id: UInt,
+    var messages: List<Message>
+)
 
 
 
